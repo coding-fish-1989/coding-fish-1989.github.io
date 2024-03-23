622e9eb1a61cda9a6b212f79c9a58d40ca100b0e
Coding Fish <154895466+coding-fish-1989@users.noreply.github.com>
2024-03-23 21:30:52 +0900

    DisplayBoy: Improve source image detection

--- displayboy/bootstrap.js
+++ displayboy/bootstrap.js
@@ -168,7 +168,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/display_boy_bg.wasm":"75a73fcb065bdb1ad433"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/display_boy_bg.wasm":"f0cab70dea8d713de7d3"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
