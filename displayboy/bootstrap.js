13b01e98f8a2de98c2f65816d7c965d593ddbeff
Coding Fish <154895466+coding-fish-1989@users.noreply.github.com>
2024-04-22 10:38:25 +0900

    DisplayBoy: Faster bilinear downsample and fix picture shift

--- displayboy/bootstrap.js
+++ displayboy/bootstrap.js
@@ -168,7 +168,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/display_boy_bg.wasm":"5a5f158ffa9fedf9fd8e"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/display_boy_bg.wasm":"ac8abe7f92316a986825"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
